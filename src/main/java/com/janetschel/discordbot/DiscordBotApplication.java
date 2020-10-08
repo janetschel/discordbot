13d6466cd7a99cef362a7c1e064765a5cbd03caa
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-10-08 10:51:27 +0200

    Little design changes

--- src/main/java/com/janetschel/discordbot/DiscordBotApplication.java
+++ src/main/java/com/janetschel/discordbot/DiscordBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscordBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DiscordBotApplication.class, args);
     }
-
 }
